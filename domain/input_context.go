1a03d75c01c2787ab1c75af48721fb67bc147914
Joao Pedro <joao-pio@hotmail.com>
2020-03-07 18:29:28 -0300

    Accept anything as input

--- domain/input_context.go
+++ domain/input_context.go
@@ -1,13 +1,5 @@
 package domain
 
-type Direction struct {
-	X float64 `json:"x"`
-	Y float64 `json:"y"`
-}
-
 type InputContext struct {
-	PlayerId      string    `json:"playerId"`
 	SessionCode   string    `json:"sessionCode"`
-	Direction     Direction `json:"direction"`
-	ActiveActions []string  `json:"activeActions"`
 }
