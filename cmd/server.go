059e425b7d45cf92dd5137028d8ef1c78b241f2d
Joao Pedro <joao-pio@hotmail.com>
2020-03-07 20:05:41 -0300

    Add healthcheck route

--- cmd/server.go
+++ cmd/server.go
@@ -4,6 +4,7 @@ import (
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 )
 
 type IServer interface {
@@ -11,6 +12,7 @@ type IServer interface {
 }
 
 type Server struct {
+	startupTime time.Time
 	middleware Middleware
 	socket     *socketio.Server
 	log        *logrus.Logger
@@ -18,6 +20,7 @@ type Server struct {
 
 func NewServer(middleware Middleware, socket *socketio.Server, logger *logrus.Logger) *Server {
 	return &Server{
+		startupTime: time.Now(),
 		middleware: middleware,
 		socket:     socket,
 		log:        logger,
@@ -28,7 +31,13 @@ func (s *Server) Run(port string) {
 	go s.socket.Serve()
 	defer s.socket.Close()
 
+	http.HandleFunc("/healthcheck", s.healthcheck)
 	http.Handle("/socket.io/", s.middleware(s.socket))
 	s.log.Println("Serving at :"+ port +"...")
 	s.log.Fatal(http.ListenAndServe(":" + port, nil))
+}
+
+func (s *Server) healthcheck(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("Application started at " + s.startupTime.Format(time.RFC3339)))
 }
